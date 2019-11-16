949340d9e7a8ecaa9c7258252b8802b330161c56
sACHIN34212 <54974924+sACHIN34212@users.noreply.github.com>
2019-11-16 23:33:56 +0530

    Add files via upload

---
+++ myphp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My PHP</title>
+</head>
+<body>
+
+	<<?php 
+
+print "Hey";
+	 ?>
+
+</body>
+</html>
---
+++ winkscript.js
@@ -0,0 +1,3 @@
+ $ setTimeout(function(){
+       $(window).location.href = 'offer.html';
+  }, 5000);
